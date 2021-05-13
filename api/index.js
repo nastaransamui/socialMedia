4610d29a71b47d09b375e7e4cbe19ad22fc429b0
Majid <nastaran.samui@gmail.com>
2021-05-13 14:01:13 +0700

    first

---
+++ api/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const mongoos = require("mongoose");
+const dotenv = require("dotenv").config();
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path");
+
+mongoos.connect(
+  process.env.MANGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  () => console.log("connected to Mango")
+);
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(8800, () => {
+  console.log(`Backend serve is running on 8800`);
+});
